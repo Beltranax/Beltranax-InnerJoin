4ae50ed85b188eeadc674cf79af7a15c8f915a7f
Beltranax <beltranangel7293@gmail.com>
2023-03-01 20:59:24 +0000

    Lab Completed

---
+++ problem1.sql
@@ -0,0 +1,4 @@
+SELECT class.teacher_name, class.class_title, student.id, student.student_name, student.class_title
+FROM student INNER JOIN class
+ON class.class_title = student.class_title
+WHERE class.teacher_name = 'Ms. Lovelace';
